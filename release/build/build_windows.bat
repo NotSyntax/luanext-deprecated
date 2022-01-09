f00c6caf3e72c7681eeaf02e8bcfc5f0bfa1d9bc
Syntax <me@uromastyx.xyz>
2022-01-09 02:06:38 -0600

    🚧 Windows Build Tools

--- release/build/build_windows.bat
+++ release/build/build_windows.bat
@@ -1,14 +1,44 @@
 :: Compile LuaNext for Windows
 :: Run this from the parent directory of release/build
+@echo off
+
+echo Building LuaNext
+echo Target: Windows
+echo ----------------
 
 set output=release
 set dir=./%output%/build/windows
 
+echo Cleaning %output%
+del /q %output%\\LuaNext.exe
+
 set glue=%dir%/glue.exe
 set srlua=%dir%/srlua.exe
+set lua=%dir%/lua.exe
+set squish=%dir%/squish.lua
+set build=%dir%/build.lua
 
-set command=%glue% %srlua% main.lua %output%/LuaNext.exe
+set gen=%lua% %build% main.lua -f
+set collapse=%lua% %squish% -q
+set command=%glue% %srlua% ./release/build/output.lua %output%/LuaNext.exe
 
-mkdir %output%
+echo Generating Squish file
+powershell -Command %gen%
 
+echo Renaming Squish file
+rename squishy.new squishy
+
+echo Specifying output
+echo Output './%output%/build/output.lua'>>squishy
+
+echo Squishing LuaNext
+powershell -Command %collapse%
+
+echo Compiling to .exe
 powershell -Command %command%
+
+echo Cleaning temporary files
+del squishy
+del %output%\\build\\output.lua
+
+echo Finished!
